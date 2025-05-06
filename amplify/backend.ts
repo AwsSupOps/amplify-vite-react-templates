5e87cbeecac81394e6f65f7b22e04bedc02dbe5b
EC2 Default User <ec2-user@ip-172-31-1-28.ec2.internal>
2025-05-06 19:02:37 +0000

    first2  commit

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -9,4 +9,4 @@ defineBackend({
 
 const { cfnUserPool } = backend.auth.resources.cfnResources
 // an empty array denotes "email" and "phone_number" cannot be used as a username
-cfnUserPool.usernameAttributes = []
+cfnUserPool.usernameAttributes = ["email", "preferred_username"]
